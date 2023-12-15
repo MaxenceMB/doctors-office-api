b9eb1df6c5e559ab56858d1a960c1f49200d2c8d
Ember <ember.contactme@gmail.com>
2023-12-16 00:38:36 +0100

    affichage.js : Overwrite of the submit event to control if we have entered something

--- js/affichage.js
+++ js/affichage.js
@@ -1,17 +1,39 @@
 
-const form = document.getElementById("formResearch")
 
-form.addEventListener('submit', (event) => {
+var resetOrSearchClicked;
+
+function checkValidPatient() {
 	const toulouseFilter = document.getElementById("toulouse")
 	const civiliteFilter = document.getElementById("civilite")
 	const medecinTraitantFilter = document.getElementById("medecinTraitant")
 	const input = document.getElementById("searchinput")
 
-	if (toulouseFilter.options[toulouse.selectedIndex].text === "Indifférent" && 
-		toulouseFilter.options[toulouse.selectedIndex].text === "Indifférent" &&
-		toulouseFilter.options[toulouse.selectedIndex].text === "Indifférent") {
-			alert('test')
+	if (resetOrSearchClicked === ) {
+
 	}
 
+	if (toulouseFilter.options[toulouseFilter.selectedIndex].text === "Indifférent" && 
+		civiliteFilter.options[civiliteFilter.selectedIndex].text === "Indifférent" &&
+		medecinTraitantFilter.options[medecinTraitantFilter.selectedIndex].text === "Indifférent" && 
+		input.value === "") {
+			document.getElementsByClassName("nbResultat")[0].innerText = "Merci de renseigner une recherche"
+			document.getElementsByClassName("nbResultat")[0].style.color = "red"
+			return false;	
+	}
+	document.getElementsByClassName("nbResultat")[0].style.color = "none"
+	return true
+}
 
-})
+function checkValidMedecin() {
+	const civiliteFilter = document.getElementById("civilite")
+	const input = document.getElementById("searchinput")
+
+	if (civiliteFilter.options[civiliteFilter.selectedIndex].text === "Indifférent" &&
+		input.value === "") {
+			document.getElementsByClassName("nbResultat")[0].innerText = "Merci de renseigner une recherche"
+			document.getElementsByClassName("nbResultat")[0].style.color = "red"
+			return false;	
+	}
+	document.getElementsByClassName("nbResultat")[0].style.color = "none"
+	return true
+}
